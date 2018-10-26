251313d3c542e95b1e7f86189f5b30df5a390c33
Graham Ollis <plicease@cpan.org>
2018-10-26 15:55:47 -0400

    Initial commit

---
+++ lib/Test2/Tools/PerlTidy.pm
@@ -0,0 +1,10 @@
+package Test2::Tools::PerlTidy;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Test2 check that all of your Perl files are tidy
+# VERSION
+
+1;
---
+++ t/test2_tools_perltidy.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Test2::Tools::PerlTidy;
+
+ok 1, 'todo';
+
+done_testing
